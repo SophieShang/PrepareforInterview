2d566a54dace23a7da06bf1847dac0f50ae08838
Tianxiao Gu <tianxiao.gu@gmail.com>
2014-12-25 23:21:57 +0800

    Sync

--- src/com/basic/datastructure05/SelectK.java
+++ src/com/basic/datastructure05/SelectK.java
@@ -36,32 +36,53 @@ public class SelectK {
 	}
 	
 	public int findK(int[] m, int mbegin, int mend, int[] n, int nbegin, int nend, int k){
-		int mmid = (mbegin+mend)/2;
+        if(mbegin > mend && nbegin > nend){
+            throw new IllegalStateException("Invalid State");
+        }
+
+        if (mbegin > mend){
+            return n[k-1+nbegin];
+        }
+
+        if (nbegin > nend){
+            return m[k-1+mbegin];
+        }
+
+        int mmid = (mbegin+mend)/2;
 		int l1 = mmid - mbegin + 1;
 		int nmid = (nbegin+nend)/2;
 		int l2 = nmid - nbegin + 1;
-		while(mbegin < mend && nbegin < nend){
-			if(m[mmid] < n[nmid]){
-				if(l1+l2 > k){
-					return findK(m, mbegin, mend, n, nbegin, nmid, k);
-				}else{
-					return findK(m, mmid, mend, n, nbegin, nend, k-l1);
-				}
-			}
-			if(m[mmid] >= n[nmid]){
-				if(l1+l2 > k){
-					return findK(m, mbegin, mmid, n, nbegin, nmid, k);
-				}else{
-					return findK(m, mbegin, mend, n, nmid, nend, k-l2);
-				}
-			}
-		}
-		return -1;
+
+        // mid divide each array into two parts, A or B,
+        // then, we have 4 parts, mA, mB, nA, nB
+        //
+
+        if (m[mmid] < n[nmid]) {
+            if (l1 + l2 > k) { // l1+l2>=k+1, n[nmid] must not be the kth, exclude it
+                // no nB
+                return findK(m, mbegin, mend, n, nbegin, nmid-1, k);
+            } else {
+                // no mA
+                return findK(m, mmid, mend, n, nbegin, nend, k - l1);
+            }
+        }
+        //if(m[mmid] >= n[nmid]){
+        else {
+            if (l1 + l2 > k) {
+                // no mB
+                return findK(m, mbegin, mmid - 1, n, nbegin, nend, k);
+            } else {
+                // no nA
+                return findK(m, mbegin, mend, n, nmid, nend, k - l2);
+            }
+        }
+
+        //return -1;
 	}
 	
 	public static void main(String[]args){
-		int [] m = {0,1,2,3};
-		int [] n = {2,3,4};
+		int [] m = {0,2,4,6};
+		int [] n = {1,3,5};
 		/*System.out.println(nativeSelectK(m,n,1));
 		System.out.println(nativeSelectK(m,n,2));
 		System.out.println(nativeSelectK(m,n,3));
