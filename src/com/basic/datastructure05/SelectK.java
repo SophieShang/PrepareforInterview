22dc9438ee132caf38ba44a4a4d2039f377cd243
Sophie Shang <shangxing_great@163.com>
2014-12-25 09:15:42 -0500

    select k

--- src/com/basic/datastructure05/SelectK.java
+++ src/com/basic/datastructure05/SelectK.java
@@ -35,13 +35,41 @@ public class SelectK {
 		return -1;
 	}
 	
+	public int findK(int[] m, int mbegin, int mend, int[] n, int nbegin, int nend, int k){
+		int mmid = (mbegin+mend)/2;
+		int l1 = mmid - mbegin + 1;
+		int nmid = (nbegin+nend)/2;
+		int l2 = nmid - nbegin + 1;
+		while(mbegin < mend && nbegin < nend){
+			if(m[mmid] < n[nmid]){
+				if(l1+l2 > k){
+					return findK(m, mbegin, mend, n, nbegin, nmid, k);
+				}else{
+					return findK(m, mmid, mend, n, nbegin, nend, k-l1);
+				}
+			}
+			if(m[mmid] >= n[nmid]){
+				if(l1+l2 > k){
+					return findK(m, mbegin, mmid, n, nbegin, nmid, k);
+				}else{
+					return findK(m, mbegin, mend, n, nmid, nend, k-l2);
+				}
+			}
+		}
+		return -1;
+	}
+	
 	public static void main(String[]args){
-		int [] m = {1,2,3};
+		int [] m = {0,1,2,3};
 		int [] n = {2,3,4};
-		System.out.println(nativeSelectK(m,n,1));
+		/*System.out.println(nativeSelectK(m,n,1));
 		System.out.println(nativeSelectK(m,n,2));
 		System.out.println(nativeSelectK(m,n,3));
 		System.out.println(nativeSelectK(m,n,4));
+*/		
+		SelectK sk = new SelectK();
+		System.out.println(sk.findK(m, 0, 3, n, 0, 2, 3));
+		
 	}
 
 }
