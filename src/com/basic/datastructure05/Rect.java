a0ef5010658db799e84996621f04bf4f47cfb1b4
Sophie Shang <shangxing_great@163.com>
2014-12-27 10:22:00 -0500

    leetcode

---
+++ src/com/basic/datastructure05/Rect.java
@@ -0,0 +1,45 @@
+package com.basic.datastructure05;
+
+public class Rect extends Shape{
+	private double length;
+	private double width;
+	
+	public Rect(double length, double width){
+		this.length = length;
+		this.width = width;
+	}
+	
+	public void setLength(double leng){
+		this.length = leng;
+	}
+	
+	public double getLength(){
+		return length;
+	}
+	
+	public void setWidth(double wid){
+		this.width = wid;
+	}
+	
+	public double getWidth(){
+		return width;
+	}
+
+	@Override
+	public double getAcreage() {
+		return length * width;
+	}
+
+	@Override
+	public double getPerimeter() {
+		return 2*(length+width);
+	}
+	
+	public static void main(String[] args){
+		Shape re = new Rect(9,5);   //父类指针指向子类对象，向下转型，多态的体现
+		System.out.println("矩形的面积是：" + re.getAcreage());
+		System.out.println("矩形的周长是：" + re.getPerimeter());
+		
+	}
+
+}
---
+++ src/com/basic/datastructure05/Shape.java
@@ -0,0 +1,7 @@
+package com.basic.datastructure05;
+
+public abstract class Shape {
+	public abstract double getAcreage();
+	public abstract double getPerimeter();
+
+}
