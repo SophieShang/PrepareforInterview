214c1e91d47cf63da7c4d924e95201a51d0ad7ba
Sophie Shang <shangxing.great@gmail.com>
2014-12-28 00:35:31 +0800

    add numbers

---
+++ src/com/basic/supsub/Super.java
@@ -0,0 +1,25 @@
+package com.basic.supsub;
+
+ class Super {
+	Super(int i){
+		
+	}
+	
+	Super(){
+		
+	}
+	
+
+
+	 private class A{
+		 
+	 }
+	
+	class Child extends Super{
+	public Child(int i){
+			super();
+		}
+	}
+}
+
+
